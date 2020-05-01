b5ccac3cb173343f0df64dc80e77f69c40b17733
StephenLee <hellenism@qq.com>
2020-05-02 05:50:36 +0800

    完成导航

--- ZyToyShop/miniprogram/custom-tab-bar/index.js
+++ ZyToyShop/miniprogram/custom-tab-bar/index.js
@@ -4,25 +4,25 @@ Component({
     color: "#7A7E83",
     selectedColor: "#3cc51f",
     list: [{
-      pagePath: "/index/index",
-      iconPath: "/image/icon_component.png",
-      selectedIconPath: "/image/icon_component_HL.png",
+      pagePath: "/miniprogram/pages/toyshopPages/mainPage/mainPage",
+      iconPath: "./images/icon_API.png",
+      selectedIconPath: "./images/icon_API_HL.png",
       text: "首页"
     }, {
       pagePath: "/index/index2",
-      iconPath: "/image/icon_API.png",
-      selectedIconPath: "/image/icon_API_HL.png",
-      text: "商场"
+      iconPath: "./images/icon_API.png",
+      selectedIconPath: "./images/icon_API_HL.png",
+      text: "商城"
     },
     {
       pagePath: "/index/index2",
-      iconPath: "/image/icon_API.png",
-      selectedIconPath: "/image/icon_API_HL.png",
+      iconPath: "./images/icon_API.png",
+      selectedIconPath: "./images/icon_API_HL.png",
       text: "购物车"
     }, {
       pagePath: "/index/index2",
-      iconPath: "/image/icon_API.png",
-      selectedIconPath: "/image/icon_API_HL.png",
+      iconPath: "./images/icon_API.png",
+      selectedIconPath: "./images/icon_API_HL.png",
       text: "个人中心"
     }]
   },
